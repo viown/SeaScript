6e76f2928a405f298c36bc4293c41d3b5e275377
viown <48097677+viown@users.noreply.github.com>
2021-05-31 07:54:09 +0300

    Fixed read_file

--- src/seascript.c
+++ src/seascript.c
@@ -8,8 +8,19 @@
 
 #include "./debug.h"
 
+char* read_file(const char* path) {
+	FILE *fp = fopen(path, "r");
+	char* source = malloc(get_file_size(fp) + 1);
+	if (fp != NULL) {
+		size_t len = fread(source, sizeof(char), get_file_size(fp), fp);
+		source[len++] = '\0';
+		fclose(fp);
+	}
+	return source;
+}
+
 int main() {
-	char* source = "global a = 15; global b = 20;";
+	char* source = read_file("tests\\parser_test.ss");
 	// lex test
 	lex_Object object;
 	lexObject_init(&object, source);
